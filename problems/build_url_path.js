03645c92ccc4ef62f4ab80dd2c271dbd1cc43ec0
m.emre yalçın <99.emreyalcin@gmail.com>
2022-09-11 02:27:46 +0300

    update: build_url_path

--- problems/build_url_path.js
+++ problems/build_url_path.js
@@ -1,10 +1,17 @@
 function buildPath(path, payload) {
-  let [url, query] = path.split('?')
-  Object.entries(payload).forEach(([k, v]) => {
-    url = url.replace(`/:${k}`, `/${v}`)
-    query = query.replace(`{${k}}`, v)
+  return path.replace(/(:.*?(?=[/?]))|({.*?})/ig, (m, o, s) => {
+    m = m.replace(/[:{}]/g, '')
+    return m ? payload[m] : o
   })
-  return url + '?' + query
 }
+
+// function buildPath (path, payload) {
+//   let [url, query] = path.split('?')
+//   Object.entries(payload).forEach(([k, v]) => {
+//     url = url.replace(`/:${k}`, `/${v}`)
+//     query = query.replace(`{${k}}`, v)
+//   })
+//   return url + '?' + query
+// }
 
 console.log(buildPath("https://www.abc.com/:sender/:id?x={x}", { sender: "emre", id: 5, x: 2 }))
